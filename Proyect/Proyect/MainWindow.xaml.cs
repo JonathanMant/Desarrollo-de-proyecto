51f1e425b975b2239bdcc619f277e7a1a7304132
Jonathan Mantilla <jonathan_mantilla@hotmail.com>
2020-01-09 20:50:12 -0500

    Desarrollo en proyecto

---
+++ Proyect/Proyect/MainWindow.xaml.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Data;
+using MySql.Data.MySqlClient;
+
+namespace Proyect
+{
+    /// <summary>
+    /// Lógica de interacción para MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            
+        }
+
+            MySqlConnection sqlcon = new MySqlConnection("datasource=127.0.0.1;username=root;password=;database=proyect");
+
+        
+
+
+        private void button_Click(object sender, RoutedEventArgs e)
+        {
+          
+            try
+            {
+                if (sqlcon.State == ConnectionState.Closed)
+                {
+                    sqlcon.Open();
+                    String consulta = "select count(1) from tbusuarios where login=@username and password= @password";
+                    MySqlCommand sqlcmd = new MySqlCommand(consulta, sqlcon);
+                    sqlcmd.CommandType = CommandType.Text;
+                    sqlcmd.Parameters.AddWithValue("@username", txtuser.Text);
+                    sqlcmd.Parameters.AddWithValue("@password", txtpass.Password);
+                    int count = Convert.ToInt32(sqlcmd.ExecuteScalar());
+                    if (count == 1)
+                    {
+                        
+                        Menu dashboard = new Menu();
+                        dashboard.Show();
+                        this.Close();
+                        
+                    }
+                    else
+                    {
+                        MessageBox.Show("Usuario o password incorrecto");
+                    }
+
+                }
+                
+
+            }catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                sqlcon.Close();
+            }
+
+        }
+    }
+}
---
+++ Proyect/Proyect/Menu.xaml.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using System.Runtime.InteropServices;
+namespace Proyect
+{
+    /// <summary>
+    /// Lógica de interacción para Menu.xaml
+    /// </summary>
+    public partial class Menu : Window
+    {
+        public Menu()
+        {
+            InitializeComponent();
+        }
+
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            if (Menuvertical.Width == 150 && panmenu.Width == 682)
+            {
+                Menuvertical.Width = 59;
+                panmenu.Width = 778;
+
+                
+            }
+            else
+            {
+                Menuvertical.Width = 150;
+                panmenu.Width = 682;
+            }
+
+        }
+
+        private void Btnpeq_Click(object sender, RoutedEventArgs e)
+        {
+            this.WindowState = WindowState.Minimized;
+        }
+
+        private void Btncerrar_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+      
+    }
+
+    
+    
+}
